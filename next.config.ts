48a0204e148b856e3068a3b8d1e1c5e8711913dd
Paul Lewis <lewi0231@flinders.edu.au>
2025-11-04 06:44:02 +1030

    next-config added cacheComponents and removed console logs.

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    cacheComponents: true,
+    optimizeCss: true,
+  },
+  compiler: {
+    removeConsole: process.env.NODE_ENV === "production",
+  },
 };
 
 export default nextConfig;
